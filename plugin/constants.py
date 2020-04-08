1062bf0468a0d4478c5764a41e5bdc3f10550405
Krzysztof Frukacz <krzysztof.frukacz@cloudify.co>
2020-04-08 10:23:01 +0200

    Copied private function from workflows
    Added a loop to remove update supporting instances from reinstall list

---
+++ plugin/constants.py
@@ -0,0 +1,5 @@
+PREUPDATE_INTERFACE = 'cloudify.interfaces.preupdate'
+UPDATE_INTERFACE = 'cloudify.interfaces.update'
+POSTUPDATE_INTERFACE = 'cloudify.interfaces.postupdate'
+
+UPDATE_OPERATION = UPDATE_INTERFACE + '.update'
